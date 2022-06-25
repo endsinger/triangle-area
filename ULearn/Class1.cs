1931d2ee6b074e060eb4ce21a279b8c8fa882337
endsinger <102259704+endsinger@users.noreply.github.com>
2022-06-25 16:34:30 +0400

    Add files via upload

---
+++ ULearn/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULearn
+{
+    class Class1
+    {
+       public static void GetInput(int input)
+        {
+            if (input == 1)
+            {
+                Console.WriteLine(" Введите значения двух сторон и угла");
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                DveStoronyUgol(a, b, y);
+
+            } 
+            else if (input == 2)
+            {
+                Console.WriteLine(" Введите значения основания и высоты");
+                double b = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                Osnovanie(b, h);
+            }
+            else if (input == 3)
+            {
+                Console.WriteLine(" Введите значения трех строн треугольника");
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                Geron(a, b, c);
+            }
+        }
+        public static void Geron(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double s = p * (p - a) * (p - b) * (p - c);
+            double sFinally = Math.Sqrt(s);
+            Console.WriteLine($"Площадь равна: {sFinally}");
+        }
+
+        public static void DveStoronyUgol(double a, double b, double y)
+        {
+            double s = 0.5 * a * b * Math.Sin(y);
+            Console.WriteLine($"Площадь равна: {s}");
+        }
+        public static void Osnovanie(double b, double h)
+        {
+            double s = 0.5 * b * h;
+            Console.WriteLine($"Площадь равна: {s}");
+        }
+    }
+}
---
+++ ULearn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ULearn
+{
+    class Program
+    {      
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("========================== \n \n Что вы хотите найти? \n Площадь треугольника через две сороны и угол между ними введите - 1 \n Через основание и высоту введите - 2 \n Через формулу Герона - 3 ");
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    Class1.GetInput(input);                 
+                }
+                catch
+                {
+                    Console.WriteLine("Проверьте правильность написания");
+                }
+            }
+        }
+
+    }
+
+}
